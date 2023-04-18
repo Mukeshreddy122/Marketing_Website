dca81ef0fc188f35ff231f324fb037a97b417540
mukesh <mukeshvancha122@gmail.com>
2023-04-19 00:37:28 +0530

    nav and footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-
-import './App.css';
+import "./App.css";
+import About from "./Sections/About";
+import Contact from "./Sections/Contact";
+import Experience from "./Sections/Experience";
+import Home from "./Sections/Home";
+import Skills from "./Sections/Skills";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
-     Hello
+      <Navbar />
+      <Home />
+      <Skills />
+      <About />
+      <Experience />
+      <Contact />
+      <Footer/>
     </div>
   );
 }
